b631641355e5c2958a2b8f6146925d6463e790f1
Faatimah <faatimahmansoor@gmail.com>
2020-02-18 14:55:42 +0200

    Added some docstrings for functions 3, 5 and 6

--- myfunctions/function3.py
+++ myfunctions/function3.py
@@ -1,4 +1,5 @@
 def date_parser(dates):
+    """Takes an input of a list of datetime strings and returns a list of the date only in `'yyyy-mm-dd'` format."""
     new_dates = []
     for i in dates:
         new_dates.append(i.split(' ')[0])
--- myfunctions/function6.py
+++ myfunctions/function6.py
@@ -1,3 +1,6 @@
 def word_splitter(df):
+    """Takes a pandas dataframe as an input, splits sentences from the 'Tweets' column and places these sentences into a new column called 
+    'Split Tweets'. 
+    """
     df['Split Tweets'] = df['Tweets'].str.lower().str.split()
     return df
